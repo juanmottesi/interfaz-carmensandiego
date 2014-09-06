504420aed56ff55d876fdf94999a203fdc514cdc
juannpm159@gmail.com <juannpm159@gmail.com@874803fa-20d0-4569-2594-fbeebde8a9c6>
2014-09-06 01:58:04 +0000

    se sacaron metodos que estaban al pedo en el detective y se ajustaron los test de este. Ademas borre todo lo que tenia main xq ya no sirve casi nada jaja despues la borramos

--- CarmenSanDiegoAppEscritorio/src/test/java/dominio/DetectiveTest.java
+++ CarmenSanDiegoAppEscritorio/src/test/java/dominio/DetectiveTest.java
@@ -1,103 +1,44 @@
 package dominio;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.uqbar.commons.model.UserException;
 
 public class DetectiveTest {
 
 	private Detective detective;
-	private Pais mockPais1;
-	private Pais mockPais2;
-	private Pais mockPais3;
-	private Villano mockVillano;
+	private Mapamundi mockMapamundi;
+	private Expediente mockExpedientes;
+	private Pais mockPais;
 	
 	@Before
 	public void setUp(){
+		mockExpedientes = mock(Expediente.class);
+		mockMapamundi = mock(Mapamundi.class);
+		mockPais = mock(Pais.class);
 		detective= new Detective();
-		mockPais1 = mock(Pais.class);
-		mockPais2 = mock(Pais.class);
-		mockPais3 = mock(Pais.class);
-		mockVillano = mock(Villano.class);
+		detective.setExpedientes(mockExpedientes);
+		detective.setMapamundi(mockMapamundi);
 	}
 	
-	@Test
-	public void testConstructor() {
-		assertNotNull(detective.getExpedientes());
-		assertNotNull(detective.getMapamundi());
-		assertNull(detective.getCasoActual());
-	}
+	
 
-	@Test
-	public void testAgregarVillanoCorrecto() {
-		detective.agregarVillano(mockVillano);
-		assertEquals(1, detective.getExpedientes().getVillanos().size());
-	}
-	
-	@Test(expected=UserException.class)
-	public void testAgregarVillanoIncorrecto() {
-		detective.agregarVillano(mockVillano);
-		detective.agregarVillano(mockVillano);
-	}
-	
-	@Test
-	public void testAgregarPaisCorrecto() {
-		detective.agregarPais(mockPais1);
-		assertEquals(1, detective.getMapamundi().getPaises().size());
-	}
-	
-	@Test(expected=UserException.class)
-	public void testAgregarPaisIncorrecto() {
-		detective.agregarPais(mockPais1);
-		detective.agregarPais(mockPais1);
-	}
 
-	@Test(expected= UserException.class)
-	public void testIniciarJuegoIncorrecto(){
-		detective.iniciarJuego();
-	}
-	
-	@Test(expected= UserException.class)
-	public void testIniciarJuegoIncorrectoConPais(){
-		detective.agregarPais(mockPais1);
-		detective.iniciarJuego();
-	}
-	
-	@Test(expected= UserException.class)
-	public void testIniciarJuegoIncorrectoConVillano(){
-		detective.agregarVillano(mockVillano);
-		detective.iniciarJuego();
-	}
-	
-	@Test(expected=UserException.class)
-	public void testIniciarJuegoCon2Paises(){
-		detective.agregarPais(mockPais1);
-		detective.agregarPais(mockPais2);
-		
-		detective.agregarVillano(mockVillano);
-		detective.iniciarJuego();
-		assertNotNull(detective.getCasoActual());
-	}
-	
-	@SuppressWarnings("unchecked")
 	@Test
 	public void testIniciarJuego(){
-		detective.agregarPais(mockPais1);
-		detective.agregarPais(mockPais2);
-		detective.agregarPais(mockPais3);
-		detective.agregarVillano(mockVillano);
-		
-		when(mockPais1.obtenerSiguientePais(anyList())).thenReturn(mockPais2);
-		when(mockPais2.obtenerSiguientePais(anyList())).thenReturn(mockPais3);
-		when(mockPais3.obtenerSiguientePais(anyList())).thenReturn(mockPais1);
-		
-		
+		ArrayList<Pais> lista = new ArrayList<Pais>();
+		lista.add(mockPais);
+		when(mockMapamundi.planDeEscape()).thenReturn(lista);
+		when(mockExpedientes.obtenerVillano()).thenReturn(new Villano());
 		detective.iniciarJuego();
-		assertNotNull(detective.getCasoActual());
 	}
 	
+
+	
+
 	
 }
--- CarmenSanDiegoAppEscritorio/src/test/java/dominio/PaisTest.java
+++ CarmenSanDiegoAppEscritorio/src/test/java/dominio/PaisTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.uqbar.commons.model.UserException;
 
 import dominio.Lugar;
-import exception.ExceptionPaisYaAgregado;
 
 public class PaisTest {
 
